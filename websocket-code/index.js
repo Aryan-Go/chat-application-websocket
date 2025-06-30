4ab1e3728f691a64071cbd34e54eb1bdd7b97828
Aryan Goyal <goyal.aryan@gmail.com>
2025-06-30 12:54:07 +0530

    [FEAT] add a websocket which is the complete logic behind the chat application
    Signed-off-by: Aryan Goyal <goyal.aryan@gmail.com>

---
+++ websocket-code/index.js
@@ -0,0 +1,17 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+let user_count = 0;
+let all_socket = [];
+wss.on("connection", (socket) => {
+    all_socket.push(socket);
+    console.log("I am in")
+    user_count = user_count+1;
+    socket.send("user is connected " + user_count);
+    socket.on("message", (e) => {
+        for (let i = 0; i < all_socket.length; i++){
+            all_socket[i].send(e.toString());
+        }
+    })
+})
+
